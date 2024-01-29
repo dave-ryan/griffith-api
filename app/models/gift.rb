e06e2683505030026d0bbbd0a0ec31e2a04a25b7
dave-a-ryan <daveryan145@gmail.com>
2024-01-28 20:08:28 -0600

    added post gifts spec

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,4 +1,7 @@
 class Gift < ApplicationRecord
   belongs_to :user
   belongs_to :purchaser, class_name: "User", foreign_key: "purchaser_id", optional: true
+
+  validates :name, presence: true
+  validates :user_id, presence: true
 end
