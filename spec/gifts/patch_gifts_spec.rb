f90999292f004bef4cb499e6f97340f77e99c8eb
dave-a-ryan <daveryan145@gmail.com>
2024-01-30 00:18:08 -0600

    added the edge cases for gift spec

--- spec/gifts/patch_gifts_spec.rb
+++ spec/gifts/patch_gifts_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe "Gifts", type: :request do
     it "returns 400" do
       FactoryBot.create(:family)
       user = FactoryBot.create(:user)
+
       request_with_login("patch", "/gifts/1", user)
       expect(response).to have_http_status(400)
     end
@@ -23,10 +24,50 @@ RSpec.describe "Gifts", type: :request do
       user = FactoryBot.create(:user)
       FactoryBot.create(:gift)
       params = { purchasing: "purchasing" }
+
       request_with_login("patch", "/gifts/1", user, params)
       expect(response).to have_http_status(200)
       data = JSON.parse(response.body)
       expect(data["purchaser_id"]).to eq user.id
+    end
+  end
+
+  describe "PATCH /gifts/1 with purchasing params and I already purchased" do
+    it "returns 200" do
+      FactoryBot.create(:family)
+      user = FactoryBot.create(:user)
+      FactoryBot.create(:gift)
+      params = { purchasing: "purchasing" }
+
+      request_with_login("patch", "/gifts/1", user, params)
+      expect(response).to have_http_status(200)
+      data = JSON.parse(response.body)
+      expect(data["purchaser_id"]).to eq user.id
+
+      request_with_login("patch", "/gifts/1", user, params)
+      expect(response).to have_http_status(200)
+      data = JSON.parse(response.body)
+      expect(data["purchaser_id"]).to eq user.id
+    end
+  end
+
+  describe "PATCH /gifts/1 with purchasing params and someone else purchased" do
+    it "returns 400" do
+      FactoryBot.create(:family)
+      user = FactoryBot.create(:user)
+      FactoryBot.create(:gift)
+      params = { purchasing: "purchasing" }
+
+      request_with_login("patch", "/gifts/1", user, params)
+      expect(response).to have_http_status(200)
+      data = JSON.parse(response.body)
+      expect(data["purchaser_id"]).to eq user.id
+
+      user2 = FactoryBot.create(:user)
+      request_with_login("patch", "/gifts/1", user2, params)
+      expect(response).to have_http_status(400)
+      data = JSON.parse(response.body)
+      expect(data["errors"]).to eq "Someone already purchased this gift!"
     end
   end
 
@@ -36,14 +77,65 @@ RSpec.describe "Gifts", type: :request do
       user = FactoryBot.create(:user)
       FactoryBot.create(:gift)
       params = { purchasing: "purchasing" }
+
       request_with_login("patch", "/gifts/1", user, params)
       data = JSON.parse(response.body)
       expect(data["purchaser_id"]).to eq user.id
+
+      params2 = { purchasing: "unpurchasing" }
+
+      request_with_login("patch", "/gifts/1", user, params2)
+      expect(response).to have_http_status(200)
+      data = JSON.parse(response.body)
+      expect(data["purchaser_id"]).to eq nil
+    end
+  end
+
+  describe "PATCH /gifts/1 with un-purchasing params but gift doesn't have purchaser" do
+    it "returns 200" do
+      FactoryBot.create(:family)
+      user = FactoryBot.create(:user)
+      FactoryBot.create(:gift)
       params = { purchasing: "unpurchasing" }
+
       request_with_login("patch", "/gifts/1", user, params)
       expect(response).to have_http_status(200)
       data = JSON.parse(response.body)
       expect(data["purchaser_id"]).to eq nil
+    end
+  end
+
+  describe "PATCH /gifts/1 with un-purchasing params but gift has a purchaser that's not me" do
+    it "returns 200" do
+      FactoryBot.create(:family)
+      user = FactoryBot.create(:user)
+      FactoryBot.create(:gift)
+      params = { purchasing: "purchasing" }
+
+      request_with_login("patch", "/gifts/1", user, params)
+      data = JSON.parse(response.body)
+      expect(data["purchaser_id"]).to eq user.id
+
+      user2 = FactoryBot.create(:user)
+      params2 = { purchasing: "unpurchasing" }
+
+      request_with_login("patch", "/gifts/1", user2, params2)
+      expect(response).to have_http_status(200)
+      data = JSON.parse(response.body)
+      expect(data["purchaser_id"]).to eq user.id
+    end
+  end
+
+  describe "PATCH /gifts/1 with name params but wrong user" do
+    it "returns 401" do
+      FactoryBot.create(:family)
+      FactoryBot.create(:user)
+      user = FactoryBot.create(:user)
+      FactoryBot.create(:gift)
+      params = { name: "Test" }
+
+      request_with_login("patch", "/gifts/1", user, params)
+      expect(response).to have_http_status(401)
     end
   end
 
@@ -53,6 +145,7 @@ RSpec.describe "Gifts", type: :request do
       user = FactoryBot.create(:user)
       FactoryBot.create(:gift)
       params = { name: "Test" }
+
       request_with_login("patch", "/gifts/1", user, params)
       expect(response).to have_http_status(200)
       data = JSON.parse(response.body)
@@ -66,6 +159,7 @@ RSpec.describe "Gifts", type: :request do
       user = FactoryBot.create(:user)
       FactoryBot.create(:gift)
       params = { link: "Test" }
+
       request_with_login("patch", "/gifts/1", user, params)
       expect(response).to have_http_status(200)
       data = JSON.parse(response.body)
