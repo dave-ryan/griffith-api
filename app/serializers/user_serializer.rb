137975779520fe8b1294a7634302424b841ec6df
dave-a-ryan <daveryan145@gmail.com>
2024-11-25 23:35:11 -0600

    fixed missing associationin serializer for secret santa users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,8 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :family, :is_admin, :santa_group, :secret_santa_id, :birthday, :share_code
 
   belongs_to :family
+  belongs_to :secret_santa, class_name: "User", foreign_key: "secret_santa_id"
+
   has_many :gifts
   has_many :customgifts
 end
